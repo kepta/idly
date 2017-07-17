710b99a7bdb2eb0891c5c39de00233c6e7c6ec0a
Kushan Joshi <0o3ko0@gmail.com>
2017-07-17 15:03:58 +0530

    Entity refactor

--- packages/idly/src/index.tsx
+++ packages/idly/src/index.tsx
@@ -7,16 +7,14 @@ import { Provider } from 'react-redux';
 import { Mapp } from 'src/components/map/index';
 import { osmReducer } from 'src/store/osm_tiles/reducer';
 import { store } from 'src/store/index';
-import * as osm from 'src/osm/entity';
-// import { osmNode } from 'src/osm/node';
+import { Entity } from 'src/osm/entity.new';
 
 require('mapbox-gl/dist/mapbox-gl.css');
 require('@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css');
 const win: any = window;
 win.Im = Im;
 win.turf = turf;
-win.osm = osm;
-// win.osmNode = osmNode;
+win.Entity = Entity;
 
 ReactDOM.render(
   <Provider store={store}>
