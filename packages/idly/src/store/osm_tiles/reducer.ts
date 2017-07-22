e1d6c66af614f53924d9837bb6444ae79a6e1233
Kushan Joshi <0o3ko0@gmail.com>
2017-07-22 10:19:28 +0530

    minor mods

--- packages/idly/src/store/osm_tiles/reducer.ts
+++ packages/idly/src/store/osm_tiles/reducer.ts
@@ -8,14 +8,12 @@ import { Action } from 'store/actions';
 import { OSM_TILES } from 'store/osm_tiles/actions';
 const initialState = {
   tiles: Map(),
-  legacy: [],
   graph: Set()
 };
 
 export class OsmTilesState extends Record(initialState) {
   public tiles: Map<string, any>;
   public graph: Set<Node | Way | Relation>;
-  public legacy: any[];
   public set(k: string, v: any): OsmTilesState {
     return super.set(k, v) as OsmTilesState;
   }
