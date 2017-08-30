71033a00fc6de2c58524d4e8f6204c37a7854189
Kushan Joshi <0o3ko0@gmail.com>
2017-08-30 10:15:00 +0530

    data: change to commonjs

--- packages/idly-data/lib/index.js
+++ packages/idly-data/lib/index.js
@@ -1,35 +1,25 @@
-(function (factory) {
-    if (typeof module === "object" && typeof module.exports === "object") {
-        var v = factory(require, exports);
-        if (v !== undefined) module.exports = v;
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const categories_json_1 = require("./data/presets/categories.json");
+const defaults_json_1 = require("./data/presets/defaults.json");
+const fields_json_1 = require("./data/presets/fields.json");
+const presets_json_1 = require("./data/presets/presets.json");
+exports.osmPavedTags = {
+    surface: {
+        paved: true,
+        asphalt: true,
+        concrete: true
+    },
+    tracktype: {
+        grade1: true
     }
-    else if (typeof define === "function" && define.amd) {
-        define(["require", "exports", "./data/presets/categories.json", "./data/presets/defaults.json", "./data/presets/fields.json", "./data/presets/presets.json"], factory);
+};
+exports.presetsData = {
+    presets: {
+        presets: presets_json_1.presets,
+        defaults: defaults_json_1.defaults,
+        categories: categories_json_1.categories,
+        fields: fields_json_1.fields
     }
-})(function (require, exports) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    const categories_json_1 = require("./data/presets/categories.json");
-    const defaults_json_1 = require("./data/presets/defaults.json");
-    const fields_json_1 = require("./data/presets/fields.json");
-    const presets_json_1 = require("./data/presets/presets.json");
-    exports.osmPavedTags = {
-        surface: {
-            paved: true,
-            asphalt: true,
-            concrete: true
-        },
-        tracktype: {
-            grade1: true
-        }
-    };
-    exports.presetsData = {
-        presets: {
-            presets: presets_json_1.presets,
-            defaults: defaults_json_1.defaults,
-            categories: categories_json_1.categories,
-            fields: fields_json_1.fields
-        }
-    };
-});
+};
 //# sourceMappingURL=index.js.map
