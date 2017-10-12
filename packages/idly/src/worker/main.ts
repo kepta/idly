00821b645cd9eeb32c9dba1e82535f867ccb15ef
Kushan Joshi <0o3ko0@gmail.com>
2017-10-12 23:08:57 +0530

    more cleanups

--- packages/idly/src/worker/main.ts
+++ packages/idly/src/worker/main.ts
@@ -3,31 +3,11 @@ import { fetchMap } from 'idly-graph/lib/operations/fetchMap';
 
 import { fetchFeatures } from 'idly-graph/lib/operations/fetchFeatures';
 import * as MyWorker from 'worker-loader!worker/worker';
-import {
-  WorkerActions,
-  WorkerActionsType,
-  WorkerFetchMap,
-  WorkerGetEntities,
-  WorkerGetFeatures
-} from 'worker/actions';
 
 const PromiseWorker = require('promise-worker');
 
 export const worker: Worker = new MyWorker();
 export const promiseWorker = new PromiseWorker(worker);
-
-function builder<W extends WorkerActionsType>() {
-  return function(type: W['type']) {
-    return function(request: W['request']): Promise<W['response']> {
-      const toSend = {
-        type,
-        request,
-        response: undefined
-      };
-      return promiseWorker.postMessage(toSend).then(r => r.response);
-    };
-  };
-}
 
 export const workerFetchMap = fetchMap(promiseWorker);
 
