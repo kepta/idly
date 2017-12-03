a8d64b793dc800b70bb4118494b8077f676db7bb
Kushan Joshi <0o3ko0@gmail.com>
2017-12-03 19:38:49 +0530

    move osm paved tags

--- packages/idly-data/src/index.ts
+++ packages/idly-data/src/index.ts
@@ -3,17 +3,6 @@ import { defaults } from '../id-data/presets/defaults.json';
 import { fields } from '../id-data/presets/fields.json';
 import { presets } from '../id-data/presets/presets.json';
 
-export const osmPavedTags = {
-  surface: {
-    paved: true,
-    asphalt: true,
-    concrete: true
-  },
-  tracktype: {
-    grade1: true
-  }
-};
-
 export const presetsData = {
   presets: {
     presets,
