29a1397f5a83ec8af5f5fe535f1870f2bd590d66
Kushan Joshi <0o3ko0@gmail.com>
2017-12-08 16:35:44 +0530

    idly-gl: insert presetMatch

--- packages/idly-gl/src/plugin/index.ts
+++ packages/idly-gl/src/plugin/index.ts
@@ -3,22 +3,27 @@ import { addSource } from '../helper/addSource';
 import {
   workerFetchMap,
   workerSetOsmTiles,
-  workerGetBbox
+  workerGetBbox,
+  workerGetEntities
 } from './worker/index';
+import { feature } from '@turf/helpers';
 import { BBox } from 'idly-common/lib/geo/bbox';
 import * as debounce from 'lodash.debounce';
-
+import { presetMatch } from 'idly-common/lib/geojson/presetMatch';
 const SOURCE_1 = 'idly-gl-src-1';
 
 export class IdlyGlPlugin {
   private map: any;
   private opts: any;
   private _container: any;
+  private selectedId: string | undefined;
   constructor(opts: { [index: string]: string }) {
     this.opts = opts;
   }
   onAdd(map: any) {
     this.map = map;
+    window.map = map;
+    window.plugin = this;
     if (this.map.loaded()) {
       this.init();
     } else {
@@ -31,6 +36,9 @@ export class IdlyGlPlugin {
   }
 
   init() {
+    this.map.on('click', this.onClick);
+    this.map.on('mousemove', this.onHover);
+
     this.map.addSource(SOURCE_1, {
       type: 'geojson',
       data: {
@@ -56,19 +64,61 @@ export class IdlyGlPlugin {
       lonLat.getEast(),
       lonLat.getNorth()
     ];
-    workerGetBbox({ bbox }).then(fc => {
-      console.log('gotdata', fc);
-      this.map.getSource(SOURCE_1).setData(fc);
+    workerSetOsmTiles({ bbox, zoom }).then(() => {
+      workerFetchMap({
+        bbox,
+        zoom
+      }).then(fc => {
+        this.map.getSource(SOURCE_1).setData(fc);
+      });
     });
-    // workerSetOsmTiles({ bbox, zoom }).then(() => {
-    //   workerFetchMap({
-    //     bbox,
-    //     zoom
-    // }).then(fc => {
-    //   console.log('gotdata');
+    // workerGetBbox({ bbox }).then(fc => {
     //   this.map.getSource(SOURCE_1).setData(fc);
     // });
-    // });
+  };
+  onClick = e => {
+    const bbox = bboxify(e, 3);
+    const features = this.map.queryRenderedFeatures(bbox);
+    const feat = features.find(f => f.properties.id);
+    if (feat && feat.properties) {
+      this.onIdSelect(feat);
+    } else {
+      this.onIdDeselect();
+    }
+  };
+
+  onIdSelect = async (feature: any) => {
+    this.selectedId = feature.properties.id;
+    if (!this.selectedId) {
+      return;
+    }
+    const shrub = await workerGetEntities({
+      entityIds: [this.selectedId]
+    });
+    const leaf = shrub.getDependant(this.selectedId);
+    // console.log(shrub);
+    window.leaf = leaf;
+    window.feature = feature;
+    if (!leaf) return;
+    window.match = presetMatch(
+      leaf.getEntity().tags,
+      feature.properties['osm_basic--geometry']
+    );
+  };
+
+  onIdDeselect = () => {
+    this.selectedId = undefined;
+  };
+
+  onHover = e => {
+    // this.map.getCanvas().style.cursor = featureId ? 'pointer' : '')
+    // );
   };
   onRemove() {}
+}
+function bboxify(e, factor) {
+  return [
+    [e.point.x - factor, e.point.y - factor],
+    [e.point.x + factor, e.point.y + factor]
+  ];
 }
