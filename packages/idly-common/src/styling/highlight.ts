f7accbd58dd567edaeef54e58c9df912dc1ea451
Kushan Joshi <0o3ko0@gmail.com>
2018-03-24 13:22:30 -0400

    add hightlight enum

---
+++ packages/idly-common/src/styling/highlight.ts
@@ -0,0 +1,7 @@
+export const enum Highlight {
+  'PRIORITY_4' = 'PRIORITY_4',
+  'PRIORITY_3' = 'PRIORITY_3',
+  'PRIORITY_2' = 'PRIORITY_2',
+  'PRIORITY_1' = 'PRIORITY_1',
+  'PRIORITY_0' = 'PRIORITY_0',
+}
