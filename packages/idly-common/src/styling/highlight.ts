5ae4ccb6eb84ee891172f433d8c1e8325e8c04d5
Kushan Joshi <0o3ko0@gmail.com>
2018-03-24 18:30:10 -0400

    add highlighting color

--- packages/idly-common/src/styling/highlight.ts
+++ packages/idly-common/src/styling/highlight.ts
@@ -1,7 +1,19 @@
 export const enum Highlight {
-  'PRIORITY_4' = 'PRIORITY_4',
-  'PRIORITY_3' = 'PRIORITY_3',
-  'PRIORITY_2' = 'PRIORITY_2',
-  'PRIORITY_1' = 'PRIORITY_1',
-  'PRIORITY_0' = 'PRIORITY_0',
+  'KIND_NEVER' = 'KIND_NEVER', // restrictions, urgent, NO!
+  'KIND_GOOD' = 'KIND_GOOD', // default, greenish, ok stuff
+  'KIND_SELECTION' = 'KIND_SELECTION', // restrictions, urgent, NO!
+  'KIND_ONLY' = 'KIND_ONLY', // Selection
+  'KIND_HOVER' = 'KIND_HOVER', // Hover
+  'KIND_NORMAL' = 'KIND_NORMAL', // ok , default, starting
+  'KIND_UNIMPORTANT' = 'KIND_UNIMPORTANT', // Unimportant
 }
+
+export const HighlightColor = {
+  [Highlight.KIND_NEVER]: '#FF0000',
+  [Highlight.KIND_GOOD]: '#32CD32',
+  [Highlight.KIND_SELECTION]: '#00f9ff',
+  [Highlight.KIND_ONLY]: '#B036FF',
+  [Highlight.KIND_NORMAL]: '#9999ff',
+  [Highlight.KIND_HOVER]: '#fbb03b',
+  [Highlight.KIND_UNIMPORTANT]: '#aaa',
+};
